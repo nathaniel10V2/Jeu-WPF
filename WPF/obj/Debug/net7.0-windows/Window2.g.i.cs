5142ef4dfffa5cb35627264d0e0d6a0b2bc94d1c
nathaniel <nathanieldayas02@gmail.com>
2024-12-17 15:16:15 +0100

    ajout de fichier

--- WPF/obj/Debug/net7.0-windows/Window2.g.i.cs
+++ WPF/obj/Debug/net7.0-windows/Window2.g.i.cs
@@ -309,7 +309,7 @@ namespace WPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WPF;component/window2.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPF;V1.0.0.0;component/window2.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Window2.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- WPF/obj/Debug/net7.0-windows/Window4.g.i.cs
+++ WPF/obj/Debug/net7.0-windows/Window4.g.i.cs
@@ -237,7 +237,7 @@ namespace WPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WPF;component/window4.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPF;V1.0.0.0;component/window4.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Window4.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
